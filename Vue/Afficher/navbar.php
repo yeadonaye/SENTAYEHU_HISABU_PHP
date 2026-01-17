2c95f1196561e2e5302c18873703f68fe397fb8e
SENTAYEHU Yeadonaye Ashenafi <yeadonaye-ashenafi.sentayehu@etu.iut-tlse3.fr>
2026-01-17 21:35:40 +0100

    Update navbar links to use absolute base path for improved navigation

--- Vue/Afficher/navbar.php
+++ Vue/Afficher/navbar.php
@@ -1,32 +1,31 @@
 <?php
-// Compute a relative prefix based on directory depth of current script
+// Base absolu: premier segment du SCRIPT_NAME (ex: /SENTAYEHU_HISABU_PHP)
 $script = str_replace('\\', '/', $_SERVER['SCRIPT_NAME'] ?? '');
-$dir = trim(dirname($script), '/');
-$depth = $dir === '' ? 0 : substr_count($dir, '/');
-$prefix = str_repeat('../', $depth);
+$parts = explode('/', trim($script, '/'));
+$base = '/' . ($parts[0] ?? '');
 ?>
 <nav class="navbar navbar-expand-lg navbar-dark sticky-top navbar-custom">
     <div class="container-fluid">
-        <a class="navbar-brand fw-bold" href="<?php echo $prefix; ?>index.php"><i class="bi bi-shield-check"></i> Gestion des Joueurs</a>
+        <a class="navbar-brand fw-bold" href="<?php echo $base; ?>/index.php"><i class="bi bi-shield-check"></i> Gestion des Joueurs</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="<?php echo $prefix; ?>index.php"><i class="bi bi-house-door"></i> Accueil</a>
+                    <a class="nav-link" href="<?php echo $base; ?>/index.php"><i class="bi bi-house-door"></i> Accueil</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="<?php echo $prefix; ?>Vue/Afficher/liste_joueurs.php"><i class="bi bi-people"></i> Joueurs</a>
+                    <a class="nav-link" href="<?php echo $base; ?>/Vue/Afficher/liste_joueurs.php"><i class="bi bi-people"></i> Joueurs</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="<?php echo $prefix; ?>Vue/Afficher/afficher_match.php"><i class="bi bi-calendar3"></i> Matchs</a>
+                    <a class="nav-link" href="<?php echo $base; ?>/Vue/Afficher/afficher_match.php"><i class="bi bi-calendar3"></i> Matchs</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="<?php echo $prefix; ?>Vue/Afficher/statistiques.php"><i class="bi bi-graph-up"></i> Statistiques</a>
+                    <a class="nav-link" href="<?php echo $base; ?>/Vue/Afficher/statistiques.php"><i class="bi bi-graph-up"></i> Statistiques</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="<?php echo $prefix; ?>logout.php"><i class="bi bi-box-arrow-right"></i> Déconnexion</a>
+                    <a class="nav-link" href="<?php echo $base; ?>/logout.php"><i class="bi bi-box-arrow-right"></i> Déconnexion</a>
                 </li>
             </ul>
         </div>
