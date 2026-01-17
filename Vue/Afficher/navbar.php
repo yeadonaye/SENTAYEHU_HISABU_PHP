ebc9181b13f790a4a64639a6949d628671f8bad3
SENTAYEHU Yeadonaye Ashenafi <yeadonaye-ashenafi.sentayehu@etu.iut-tlse3.fr>
2026-01-17 21:42:06 +0100

    correction des liens encore une fois,'là j en ai marre de ce truc'

--- Vue/Afficher/navbar.php
+++ Vue/Afficher/navbar.php
@@ -1,25 +1,25 @@
 <nav class="navbar navbar-expand-lg navbar-dark sticky-top navbar-custom">
     <div class="container-fluid">
-        <a class="navbar-brand fw-bold" href="/SENTAYEHU_HISABU_PHP/index.php"><i class="bi bi-shield-check"></i> Gestion des Joueurs</a>
+        <a class="navbar-brand fw-bold" href="/index.php"><i class="bi bi-shield-check"></i> Gestion des Joueurs</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="/SENTAYEHU_HISABU_PHP/index.php"><i class="bi bi-house-door"></i> Accueil</a>
+                    <a class="nav-link" href="/index.php"><i class="bi bi-house-door"></i> Accueil</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/SENTAYEHU_HISABU_PHP/Vue/Afficher/liste_joueurs.php"><i class="bi bi-people"></i> Joueurs</a>
+                    <a class="nav-link" href="/Vue/Afficher/liste_joueurs.php"><i class="bi bi-people"></i> Joueurs</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/SENTAYEHU_HISABU_PHP/Vue/Afficher/afficher_match.php"><i class="bi bi-calendar3"></i> Matchs</a>
+                    <a class="nav-link" href="/Vue/Afficher/afficher_match.php"><i class="bi bi-calendar3"></i> Matchs</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/SENTAYEHU_HISABU_PHP/Vue/Afficher/statistiques.php"><i class="bi bi-graph-up"></i> Statistiques</a>
+                    <a class="nav-link" href="/Vue/Afficher/statistiques.php"><i class="bi bi-graph-up"></i> Statistiques</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/SENTAYEHU_HISABU_PHP/logout.php"><i class="bi bi-box-arrow-right"></i> Déconnexion</a>
+                    <a class="nav-link" href="/logout.php"><i class="bi bi-box-arrow-right"></i> Déconnexion</a>
                 </li>
             </ul>
         </div>
