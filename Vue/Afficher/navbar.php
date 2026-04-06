92dccd166e31a69b1f4924278df52c53c46774a6
SENTAYEHU Yeadonaye Ashenafi <yeadonaye-ashenafi.sentayehu@etu.iut-tlse3.fr>
2026-04-06 23:56:09 +0200

    Correction de ce putain de navbar encore une fois

--- Vue/Afficher/navbar.php
+++ Vue/Afficher/navbar.php
@@ -1,25 +1,28 @@
-<?php $role = $_SESSION['role'] ?? 'joueur'; ?>
-<ul class="navbar-nav ms-auto">
-    <li class="nav-item">
-        <a class="nav-link" href="/index.php"><i class="bi bi-house-door"></i> Accueil</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="/Vue/Afficher/liste_joueurs.php"><i class="bi bi-people"></i> Joueurs</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="/Vue/Afficher/afficher_match.php"><i class="bi bi-calendar3"></i> Matchs</a>
-    </li>
-    <?php if ($role === 'coach'): ?>
-    <li class="nav-item">
-        <a class="nav-link" href="/Vue/Afficher/statistiques.php"><i class="bi bi-graph-up"></i> Statistiques</a>
-    </li>
-    <?php endif; ?>
-    <?php if ($role === 'joueur'): ?>
-    <li class="nav-item">
-        <a class="nav-link" href="/Vue/Afficher/statistiques.php"><i class="bi bi-graph-up"></i> Statistiques</a>
-    </li>
-    <?php endif; ?>
-    <li class="nav-item">
-        <a class="nav-link" href="/logout.php"><i class="bi bi-box-arrow-right"></i> Déconnexion</a>
-    </li>
-</ul>
+<link rel="icon" type="image/png" href="/Vue/img/logo.png">
+<nav class="navbar navbar-expand-lg navbar-dark sticky-top navbar-custom">
+    <div class="container-fluid">
+        <a class="navbar-brand fw-bold" href="/index.php"><i class="bi bi-shield-check"></i> Gestion des Joueurs</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/index.php"><i class="bi bi-house-door"></i> Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/Vue/Afficher/liste_joueurs.php"><i class="bi bi-people"></i> Joueurs</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/Vue/Afficher/afficher_match.php"><i class="bi bi-calendar3"></i> Matchs</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/Vue/Afficher/statistiques.php"><i class="bi bi-graph-up"></i> Statistiques</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/logout.php"><i class="bi bi-box-arrow-right"></i> Déconnexion</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
